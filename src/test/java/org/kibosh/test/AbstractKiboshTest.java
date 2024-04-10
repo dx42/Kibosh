41c70dad0df70b691c849f5b0588a017ebdc148c
Chris Mair <cmair42@gmail.com>
2024-04-09 20:28:41 -0400

    AbstractKiboshTest

---
+++ src/test/java/org/kibosh/test/AbstractKiboshTest.java
@@ -0,0 +1,18 @@
+package org.kibosh.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class AbstractKiboshTest {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    protected static <T> List<T> list(T... elements) {
+        return Arrays.stream(elements).collect(Collectors.toList());
+    }
+
+}
